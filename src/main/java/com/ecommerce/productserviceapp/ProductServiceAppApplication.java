ba02a9a2b81e2993347db8618fa889efd1da370f
sree <plssreedhar@gmail.com>
2025-05-27 23:20:13 +0530

    created a new spring boot repo

---
+++ src/main/java/com/ecommerce/productserviceapp/ProductServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.productserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecommerce/productserviceapp/ProductServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.productserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
