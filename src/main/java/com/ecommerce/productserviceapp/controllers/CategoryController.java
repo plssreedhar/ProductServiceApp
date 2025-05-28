336b738ac511c270069042644ff06743aa66ada2
sree <plssreedhar@gmail.com>
2025-05-28 09:16:24 +0530

    Added CategoryController and a CategoryService with just empty methods

---
+++ src/main/java/com/ecommerce/productserviceapp/controllers/CategoryController.java
@@ -0,0 +1,30 @@
+package com.ecommerce.productserviceapp.controllers;
+
+import com.ecommerce.productserviceapp.models.Category;
+import com.ecommerce.productserviceapp.services.CategoryService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategory(@PathVariable("id") Long categoryId) {
+       return categoryService.getCategory(categoryId);
+    }
+
+    @PostMapping("/")
+    public Category addCategory(@RequestParam("title") String title){
+        return categoryService.addCategory(title);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable("id") Long categoryId) {
+        categoryService.deleteCategory(categoryId);
+    }
+}
---
+++ src/main/java/com/ecommerce/productserviceapp/services/CategoryService.java
@@ -0,0 +1,18 @@
+package com.ecommerce.productserviceapp.services;
+
+import com.ecommerce.productserviceapp.models.Category;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+    public void deleteCategory(Long categoryId) {
+    }
+
+    public Category getCategory(Long categoryId) {
+        return null;
+    }
+
+    public Category addCategory(String title) {
+        return null;
+    }
+}
